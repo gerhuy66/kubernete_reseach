acad440863ea89c6037568191d8471bc311114d7
lcdhuy <56525939+gerhuy66@users.noreply.github.com>
2020-10-09 13:38:35 +0700

    Update main.go

--- hello-app/main.go
+++ hello-app/main.go
@@ -1,72 +1,51 @@
-/**
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// [START container_hello_app]
 package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
+	"encoding/json"
 	"html/template"
 )
+
 type User struct  {
 	Id int 		 `json:"id"`
 	Name string	 `json:"name"`
 	Email string `json:"email"`
 	Phone string `json:"phone"`
 }
+
 func main() {
-	// register hello function to handle all requests
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/json", jsonHandler)
 	http.HandleFunc("/template", templateHandler)
-
-	// use PORT environment variable, or default to 8080
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	// start the web server on port and accept requests
-	log.Printf("Server listening on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	http.ListenAndServe(":8080", nil)
 }
 
-// hello responds to the request with a plain-text "Hello, world" message.
-func hello(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Serving request: %s", r.URL.Path)
-	host, _ := os.Hostname()
-	fmt.Fprintf(w, "<h3>Hello, world!</h3>\n")
-	fmt.Fprintf(w, "Version: 2.0.0\n")
-	fmt.Fprintf(w, "Hostname: %s\n", host)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+} 
+
+//jsonHandler returns http respone in JSON format.
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	user := User{Id: 1, 
+				Name: "John Doe", 
+				Email: "johndoe@gmail.com", 
+				Phone: "000099999"}
+	json.NewEncoder(w).Encode(user)	
 }
 
-// [END container_hello_app]
-
+//templateHandler renders a template and returns as http response.
 func templateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	t, err := template.ParseFiles("template.html")
 	if err != nil {
 		fmt.Fprintf(w, "Unable to load template")
 	}
+
 	user := User{Id: 1, 
 		Name: "John Doe", 
 		Email: "johndoe@gmail.com", 
 		Phone: "000099999"}
-	t.Execute(w,user)
+	t.Execute(w, user)
 }
